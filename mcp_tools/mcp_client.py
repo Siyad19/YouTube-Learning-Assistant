231197f29bcfa5478005327fecad2c0f38d35327
Siyad19 <siyads6238@gmail.com>
2026-08-31 10:50:01 +0530

    MCP client fixed

--- mcp_tools/mcp_client.py
+++ mcp_tools/mcp_client.py
@@ -1,40 +1,52 @@
-import asyncio
+from pathlib import Path
+import sys
+
 from mcp import ClientSession
 from mcp.client.stdio import stdio_client, StdioServerParameters
 
+
+SERVER_PATH = Path(__file__).parent / "yt_mcp_server.py"
+
+
 async def fetch_transcript(yt_link: str):
 
-    server =StdioServerParameters(
-        command="python",
-        args=["mcp_tools/yt_mcp_server.py"]
+    server = StdioServerParameters(
+        command=sys.executable,
+        args=[str(SERVER_PATH)]
     )
 
     async with stdio_client(server) as (read, write):
         async with ClientSession(read, write) as session:
+
             await session.initialize()
+
             result = await session.call_tool(
-                "get_transcript", 
+                "get_transcript",
                 {
                     "yt_url": yt_link
                 }
             )
+
             return result.content[0].text
 
 
 async def fetch_description(yt_link: str):
 
-    server =StdioServerParameters(
-            command="python",
-            args=["mcp_tools/yt_mcp_server.py"]
+    server = StdioServerParameters(
+        command=sys.executable,
+        args=[str(SERVER_PATH)]
     )
 
     async with stdio_client(server) as (read, write):
         async with ClientSession(read, write) as session:
+
             await session.initialize()
+
             result = await session.call_tool(
-                "video_description", 
-                    {
-                        "yt_url": yt_link
-                    }
+                "video_description",
+                {
+                    "yt_url": yt_link
+                }
             )
+
             return result.content[0].text
